31375d6efc252fdc1e2d19c7b1c0cb6d388c6e2b
Egor Aryamov <eariamov.exceedteam@gmail.com>
2021-06-02 09:34:02 +0300

    destruct project according to the development standart

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const apiRoutes = require('./src/modules/routes/routes');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', apiRoutes);
+
+
+const url = 'mongodb+srv://EgorRestAPI:Nokiaversia228@cluster0.qnjrd.mongodb.net/UsersDB?retryWrites=true&w=majority';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+app.listen(8000, () => {
+  console.log('Server has been started on port 8000...')
+})
---
+++ src/modules/controllers/task.controllers.js
@@ -0,0 +1,41 @@
+const Task = require('../../db/models/task/index')
+
+module.exports.getAllTasks = (req, res, next) => { 
+    Task.find().then(result => {
+      res.send({data: result});
+    })
+  }
+  
+module.exports.createNewTask = (req, res, next) => {
+    const body = req.body;
+    const task = new Task(body);
+    task.save(body).then(() => {
+      Task.find().then(result =>{
+        res.send({data: result});
+      })
+    })
+  }
+  
+module.exports.deleteTask = (req, res, next) => {
+    Task.deleteOne({_id: req.query._id}).then(() => {
+      Task.find().then(result => {
+        res.send({data: result});
+      })
+    })
+  }
+  
+  module.exports.changeTaskInfo = (req, res, next) => {
+    Task.updateOne({_id: req.query._id}, req.body).then(() => {
+      Task.find().then(result => {
+        res.send({data: result});
+      })
+    })
+  }
+  
+module.exports.deleteAllTasks = (req, res, next) => {
+    Task.deleteMany().then(() => {
+      Task.find().then(result => {
+        res.send({data: result});
+      })
+    })
+  }
