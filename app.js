11f79f55ac4addbc32f1addf8c4a53ab03686a52
Egor Aryamov <eariamov.exceedteam@gmail.com>
2021-06-15 16:36:17 +0300

    codestyle

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const apiRoutes = require("./src/modules/routes/routes");
+
+const url = `mongodb+srv://EgorRestAPI:Nokiaversia228@cluster0.qnjrd.mongodb.net/UsersDB?retryWrites=true&w=majority`;
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(cors());
 
@@ -11,5 +20,5 @@ app.use(bodyParser.json());
 app.use("/", apiRoutes);
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log("Example app listening on port 8000!");
 });
